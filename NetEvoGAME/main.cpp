0e3072a6611dbacfefdf6f8eeb0629cdf1c7f08a
BildPeter <BildPeter@web.de>
2012-10-23 14:59:07 +0200

    Initial Commit

---
+++ NetEvoGAME/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NetEvoGAME
+//
+//  Created by Peter on 23.10.12.
+//  Copyright (c) 2012 Peter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
