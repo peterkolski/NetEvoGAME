310db27524e317c6945bc66c7217bb8359daf658
BildPeter <BildPeter@web.de>
2013-01-07 16:44:27 +0100

    Adding edges works

--- NetEvoGAME/BarabasiAlbert_Generator.cpp
+++ NetEvoGAME/BarabasiAlbert_Generator.cpp
@@ -9,7 +9,14 @@
 
 #include <netevo.h>
 #include <iostream>
-//#include <lemon/list_graph.h>
+#include <lemon/full_graph.h>
+#include <lemon/edge_set.h>
+
+/*
+    ToDo:
+        Random seed;
+        Calculate the probability
+ */
 
 using namespace std;
 using namespace lemon;
@@ -18,6 +25,46 @@ using namespace netevo;
 int main( void ){
 
     
+    int init_num_nodes, max_nodes, edge_addition_num;
     
+    init_num_nodes      = 10;
+    max_nodes           = 100;
+    edge_addition_num   = 7;
     
+    typedef ListEdgeSet< ListGraph >    EdgeSet;
+    ListGraph                           mListGraph;
+    EdgeSet                             mNewEdges( mListGraph );
+    FullGraph                           fg(init_num_nodes);
+    
+    GraphCopy<FullGraph, ListGraph>     cg( fg, mListGraph); // Create the seed nodes
+    cg.run();
+    
+    int randomNodeID = -1;
+    int mNumEdges   = -1;
+    int mNumNodes   = -1;
+    EdgeSet::Edge e;
+    lemon::Random   mRandom;
+    ListGraph::Node newNode;
+    
+    for ( int i = init_num_nodes; i < max_nodes; i++){
+        newNode     = mListGraph.addNode();
+        mNumEdges   = countEdges( mListGraph );
+        mNumNodes   = countNodes( mListGraph );
+        mNewEdges.clear();
+        
+        while ( countEdges( mNewEdges ) != edge_addition_num ) {
+            randomNodeID = mRandom.integer(0, mNumNodes - 1);
+            e = findEdge( mNewEdges, newNode, mNewEdges.nodeFromId( randomNodeID ) ); // does it exist already?
+            if ( e == INVALID){
+                mNewEdges.addEdge(  newNode, mListGraph.nodeFromId( randomNodeID ) );
+            }
+        }
+        // Create the new edges
+        for (EdgeSet::EdgeIt e( mNewEdges ); e!=INVALID; ++e){
+            mListGraph.addEdge( mNewEdges.u( e ), mNewEdges.v(e) );
+        }
+    }
+    
+    cout << countEdges( mListGraph) << endl;
+    cout << countEdges( fg ) << endl;
 }
